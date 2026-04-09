4c9ffe0565ac7d31749dd3980d9aa28997ae9b28
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2026-04-09 09:46:19 -0300

    feat: add debug variables to index.html to make it easier to understand current configurations

--- internal/handler/template.go
+++ internal/handler/template.go
@@ -18,9 +18,17 @@ func Index(c *gin.Context) {
 
 	isBotOn := storage.GetBotIsOn()
 
+	currentTwitchBroadcasterID := env.TwitchBroadcasterID
+	currentTwitchBotUserID := env.TwitchBotUserID
+	currentTwitchBotUserName := env.TwitchBotUserName
+
 	c.HTML(http.StatusOK, "index.html", gin.H{
 		"title":      env.AppName,
 		"isLoggedIn": IsLoggedIn,
 		"isBotOn":    isBotOn,
+
+		"currentTwitchBroadcasterID": currentTwitchBroadcasterID,
+		"currentTwitchBotUserID":     currentTwitchBotUserID,
+		"currentTwitchBotUserName":   currentTwitchBotUserName,
 	})
 }
