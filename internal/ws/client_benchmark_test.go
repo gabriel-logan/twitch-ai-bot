83eecf557553cc0381457cfa7ad0ff03dbed48e9
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2026-04-14 11:02:05 -0300

    test: add benchmarks for passing config.Env by value and pointer through nested functions

--- internal/ws/client_benchmark_test.go
+++ internal/ws/client_benchmark_test.go
@@ -213,3 +213,37 @@ func BenchmarkAccessAllFieldsPtr(b *testing.B) {
 		sinkDuration = env.ContextRequestDuration
 	}
 }
+
+func BenchmarkPassEnvValueThroughLayers(b *testing.B) {
+	env := setupEnvValue()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		func(env config.Env) {
+			func(env config.Env) {
+				func(env config.Env) {
+					func(env config.Env) {
+						consumeEnvValue(env)
+					}(env)
+				}(env)
+			}(env)
+		}(env)
+	}
+}
+
+func BenchmarkPassEnvPtrThroughLayers(b *testing.B) {
+	env := setupEnvPtr()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		func(env *config.Env) {
+			func(env *config.Env) {
+				func(env *config.Env) {
+					func(env *config.Env) {
+						consumeEnvPtr(env)
+					}(env)
+				}(env)
+			}(env)
+		}(env)
+	}
+}
